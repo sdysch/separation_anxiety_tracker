e2e7b9ead0356beefbed09ebcd0902f8a1327271
Samuel Dezso Dysch <sdysch94@gmail.com>
2025-07-30 18:25:59 +0100

    Interactive plot

--- scripts/make_summary_plot.py
+++ scripts/make_summary_plot.py
@@ -2,6 +2,7 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 conn = sqlite3.connect('sa_training.db')
 
@@ -22,6 +23,7 @@ y = 'actual_duration_mins'
 x = 'timestamp'
 
 # plot
+"""
 fig, ax = plt.subplots(figsize=(15, 5))
 
 sns.lineplot(
@@ -43,4 +45,40 @@ sns.scatterplot(
 )
 
 fig.savefig('docs/plot.png')
+"""
+
+df = df.sort_values(by=x)
+fig = px.scatter(
+    df,
+    x=x,
+    y=y,
+    symbol='rating',
+    color='rating'
+)
+
+fig.add_scatter(
+    x=df[x],
+    y=df[y],
+    mode='lines',
+    line=dict(color='gray'),
+    showlegend=False
+)
+
+# update symbol size
+fig.update_traces(marker=dict(size=14))
+
+# update font size
+fig.update_layout(
+    xaxis=dict(
+        title_font=dict(size=18),
+        tickfont=dict(size=14)
+    ),
+    yaxis=dict(
+        title_font=dict(size=18),
+        tickfont=dict(size=14)
+    )
+)
+
+# fig.show()
+fig.write_html('docs/plot.html')
 
